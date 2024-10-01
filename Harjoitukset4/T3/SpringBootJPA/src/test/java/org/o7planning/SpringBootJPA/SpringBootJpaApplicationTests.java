cb6d81fa5306026b1afa7c671692e0c287446f88
Horjamiini <54777254+Horjamiini@users.noreply.github.com>
2024-10-01 15:34:01 +0300

    T3_Transaction

---
+++ Harjoitukset4/T3/SpringBootJPA/src/test/java/org/o7planning/SpringBootJPA/SpringBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.o7planning.SpringBootJPA;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaApplicationTests {
+
+
+	void contextLoads() {
+	}
+
+}
